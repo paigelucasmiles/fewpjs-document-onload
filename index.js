51ecd27277a9e43deda0840ab086848f6980246b
Paige Miles <paige@lucas-miles.com>
2020-10-28 21:00:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", updateTextParagraph)
+
+function updateTextParagraph() {
+    const textParagraph = document.getElementById("text");
+        textParagraph.textContent = "This is really cool!";
+}
